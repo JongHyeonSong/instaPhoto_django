2d71aed703a832808dfd9f79fec99f036205702e
JongHyeonSong <mapelamll@naver.com>
2020-06-29 12:03:31 +0900

    기본인스타 입출력수정삭제 좋아요기능하기전

--- photo/urls.py
+++ photo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import PhotoCreate,PhotoDelete,PhotoUpdate,PhotoList,PhotoDetail
-
+from .views import *
 from django.http import HttpResponse
+
+
+
+
 
 def test(request):
     return HttpResponse('test.hi')
@@ -11,6 +15,10 @@ app_name = "photo"
 urlpatterns=[
     
     path("create/", PhotoCreate.as_view(), name='create'),
+    
+    path('like/<int:photo_id>/', PhotoLike.as_view(), name='like'),
+    path('favorite/<int:photo_id>/', PhotoFavorite.as_view(), name='favorite'),
+    
     path("delete/<int:pk>/", PhotoDelete.as_view(), name='delete'),
     path("update/<int:pk>/", PhotoUpdate.as_view(), name='update'),
     path("detail/<int:pk>/", PhotoDetail.as_view(), name='detail'),
--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,redirect,HttpResponseRedirect
+from django.http import HttpResponseForbidden
+from urllib.parse import urlparse
 
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic.detail import DetailView
+from django.views.generic.base import View
+from django.contrib import messages
 
 from .models import Photo
 
@@ -30,6 +34,15 @@ class PhotoUpdate(UpdateView):
     fields =['text','image']
     template_name_suffix='_update'
     success_url='/'
+    
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request,"%s님 수정권한 없습니다" % request.user.username)
+            return redirect('/')
+        else:
+            return super(PhotoUpdate,self).dispatch(request, *args, **kwargs)
+
 
 class PhotoDelete(DeleteView):
     model = Photo
@@ -37,7 +50,56 @@ class PhotoDelete(DeleteView):
     template_name_suffix='_delete'
     success_url='/'
 
+    def dispatch(self, request, *args, **kwargs):
+        object=self.get_object()
+        if object.author != request.user: #object.author > User모델로 넘어감 request.user > User모델로 넘어감
+            messages.warning(request,"삭제권한 없습니다")
+            return redirect('/')
+        else:
+                
+            return super(PhotoDelete,self).dispatch(request, *args, **kwargs)
+
 class PhotoDetail(DetailView):
     model = Photo
     template_name_suffix ='_detail'
     
+    
+
+class PhotoLike(View):
+    
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        
+        else:
+            if 'photo_id' in kwargs:
+                photo_id = kwargs['photo_id']
+                photo = Photo.objects.get(pk=photo_id)
+                user = request.user
+                if user in photo.like.all():
+                    photo.like.remove(user)
+                else:
+                    photo.like.add(user)
+            
+            referer_url = request.META.get('HTTP_REFERER')
+            path = urlparse(referer_url).path
+            print(path,'   in views')
+            return HttpResponseRedirect(path)
+            
+            
+class PhotoFavorite(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        else:
+            if 'photo_id' in kwargs:
+                photo_id = kwargs['photo_id']
+                photo =Photo.objects.get(pk=photo_id)
+                user = request.username
+                
+                if user in photo.favorite.all():
+                    photo.favorite.remove(user)
+                    
+                else:
+                    photo.favorite.add(user)
+            return HttpResponseRedirect('/')
